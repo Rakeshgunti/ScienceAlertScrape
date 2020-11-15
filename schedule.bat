1433d78cae6d1d7b242bf27333855d3f0de98802
rgunti1 <46797008+rgunti1@users.noreply.github.com>
2020-11-14 19:59:56 -0500

    Updated schedule batch to work with input arguments instead of hard-coding paths

--- schedule.bat
+++ schedule.bat
@@ -1,6 +1,9 @@
-cd C:\Users\gunti\.spyder-py3\Python-projects\ScienceAlertScrape
+@REM Please provide the project location and environment location as command line arguments when executing script or scheduling batch file
+SET arg1=%1
+cd %arg1%
+SET arg2=%2
 SET log_file=%cd%\logfile.txt
 call C:/ProgramData/Anaconda3/Scripts/activate.bat
 cd sciencealertscrape\sciencealertscrape\spiders
-C:\Users\gunti\.conda\envs\sciencescrape\python.exe SAspider.py > %log_file%
+%arg2% SAspider.py daily > %log_file%
 exit
